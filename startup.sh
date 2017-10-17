f71ef7b22507f01313fe4ad4a26d2b5bed387aff
Elian Kurtenkov <elian.kurtenkov@programista.pro>
2017-10-17 17:49:05 +0300

    Format message to Slack

--- startup.sh
+++ startup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-echo "{\"text\":\"" > tmp.txt
+searchString="Total tests:"
+url="https://hooks.slack.com/services/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"
+H="'Content-type: application/json'"
+
 b=$(dotnet vstest AFT-Assets.dll)
+
+echo "{\"attachments\": [{\"text\":\"" > tmp.txt
 b=$(echo $b | tr '\n' ' ')
 b=$(echo $b | tr '\\' "/")
 b=$(echo $b | tr "'" "“")
-echo -e $b >> tmp.txt
-echo "\"}" >> tmp.txt
 
-url="https://hooks.slack.com/services/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"
-H="'Content-type: application/json'"
+c=$(echo $b | grep -o "$searchString.*")
+
+echo $b >> tmp.txt
+echo "\"}],\"text\":\"" >> tmp.txt
+echo $c >> tmp.txt
+echo "\"}" >> tmp.txt
 
 curl -X POST -H $H --data-binary @tmp.txt $url
 
