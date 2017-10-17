caf237dad3150432d36a8c0895b77b2be38aa080
Elian Kurtenkov <elian.kurtenkov@programista.pro>
2017-10-17 16:58:39 +0300

    Change Slack URL

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ b=$(echo $b | tr "'" "“")
 echo -e $b >> tmp.txt
 echo "\"}" >> tmp.txt
 
-url="https://hooks.slack.com/services/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"
+url="https://hooks.slack.com/services/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"
 H="'Content-type: application/json'"
 
 curl -X POST -H $H --data-binary @tmp.txt $url
